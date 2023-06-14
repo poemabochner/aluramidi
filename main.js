532a0532bba6cc062d480861c91d831f67fbcf25
Poema Bochner <poemabochner8@gmail.com>
2023-06-14 07:36:48 -0300

    reprodução dos sons funcional com js

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ let contador = 0;
 while (contador < listaDeTeclas.length) {
 
   const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-  const instrumento = tecla[contador].classList[1];
-
-  tecla[contador].onclick = function () {
-    tocaSom();
+  tecla.onclick = function () {
+    tocaSom(idAudio);
   }
 
   contador = contador + 1;
